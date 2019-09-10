6869d1ecef2417af2e2a8fa9dbb37d8cfcacdb2f
ebisLab <ebaiden@techie.com>
2019-09-10 08:23:17 -0400

    delete request

--- api/accountsRouter.js
+++ api/accountsRouter.js
@@ -19,17 +19,44 @@ router.get('/:id', (req, res) => {
         .catch(err => { res.json(err) })
 })
 
+// router.post('/', (req, res) => {
+//     const accData = req.body
+//     // db.select('*').from('accounts')
+//     db('accounts')
+//         .insert(accData, 'id')
+//         .then(accRes => { res.status(200).json(accRes); })
+//         .catch(err => {
+//             console.log('error here')
+//             res.json(err)
+//         })
+// })
+
 router.post('/', (req, res) => {
     const accData = req.body
-
     // db.select('*').from('accounts')
     db('accounts')
         .insert(accData, 'id')
-        .then(accRes => { res.status(200).json(accRes); })
+        .then(([id]) => {
+
+            db('accounts').where({ id })
+                .first()
+                .then(acc => { res.status(200).json(acc) })
+            // .catch(err => { res.json(err) })
+        })
         .catch(err => {
             console.log('error here')
             res.json(err)
         })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('accounts').where({ id: req.params.id })
+        .del()
+        // .first()
+        .then(count => { res.status(200).json({ message: `deleted ${count} records` }) })
+        .catch(err => { res.json(err) })
 })
 
 module.exports = router;
